b688f8db6dc9211be9f9bbf08306ee10272c69c8
Ricky Moorhouse <ricky@samespirit.net>
2018-05-07 07:45:10 +0100

    improve test coverage

--- tests/test_hem.py
+++ tests/test_hem.py
@@ -3,63 +3,64 @@ import requests_mock
 import requests
 import datetime
 import hemApp
-class Basics(unittest.TestCase):
-        def test_check_init(self):
-            test = {'path':'/', 'secure':True, 'verify':True}
-            check = hemApp.Check('test', test)
-            assert check.url == 'https://{}/'
-            assert check.verify == True
+def test_check_init():
+    test = {'path':'/', 'secure':True, 'verify':True}
+    check = hemApp.Check('test', test)
+    assert check.url == 'https://{}/'
+    assert check.verify == True
 
-        def test_check_invoke(self):
-            with requests_mock.mock() as m:
-                m.get('https://1.1.1.1/', text="")
-                test = {'path':'/', 'secure':True, 'verify':True}
-                check = hemApp.Check('test', test)
-                results = check.test_list(["1.1.1.1"])
-                (response, timing) = results[0]
-                assert results is not None
-                assert response == 200
-                assert type(timing) is datetime.timedelta
-        def test_ssl_error(self):
-            with requests_mock.mock() as m:
-                m.get('https://1.1.1.1/', exc=requests.exceptions.SSLError)
-                test = {'path':'/', 'secure':True, 'verify':True}
-                check = hemApp.Check('test', test)
-                results = check.test_list(["1.1.1.1"])
-                (response, timing) = results[0]
-                assert results is not None
-                assert response == 526
-                assert type(timing) is datetime.timedelta
-        def test_connection_timeout(self):
-            with requests_mock.mock() as m:
-                m.get('https://1.1.1.1/', exc=requests.exceptions.ConnectTimeout)
-                test = {'path':'/', 'secure':True, 'verify':True}
-                check = hemApp.Check('test', test)
-                results = check.test_list(["1.1.1.1"])
-                (response, timing) = results[0]
-                assert results is not None
-                assert response == 522
-                assert type(timing) is datetime.timedelta
+def test_config_load():
+    conf = hemApp.load_config('tests/test_cli.yaml')
+    assert 'settings' in conf
+    assert 'discovery' in conf
 
-        def test_check_headers(self):
-            import hemApp
-            with requests_mock.mock() as m:
-                m.get('https://1.1.1.1/', text="",  request_headers={"host": "example.com"})
-                test = {'path':'/', 'secure':True, 'verify':True, "headers":{"host":"example.com"}}
-                check = hemApp.Check('test', test)
-                results = check.test_list(["1.1.1.1"])
-                (response, timing) = results[0]
-                assert results is not None
-                assert response == 200
-                assert type(timing) is datetime.timedelta
-                test = {'path':'/', 'secure':True, 'verify':True, "headers":{"host":"notexample.com"}}
-                check = hemApp.Check('test', test)
-                try:
-                    results = check.test_list(["1.1.1.1"])
-                    assert results == []
-                except requests_mock.exceptions.NoMockAddress as m:
-                    exit(m)
-                    assert "host test" is "host test"
+def test_check_invoke():
+    with requests_mock.mock() as m:
+        m.get('https://1.1.1.1/', text="")
+        test = {'path':'/', 'secure':True, 'verify':True}
+        check = hemApp.Check('test', test)
+        results = check.test_list(["1.1.1.1"])
+        (response, timing) = results[0]
+        assert results is not None
+        assert response == 200
+        assert type(timing) is datetime.timedelta
+def test_ssl_error():
+    with requests_mock.mock() as m:
+        m.get('https://1.1.1.1/', exc=requests.exceptions.SSLError)
+        test = {'path':'/', 'secure':True, 'verify':True}
+        check = hemApp.Check('test', test)
+        results = check.test_list(["1.1.1.1"])
+        (response, timing) = results[0]
+        assert results is not None
+        assert response == 526
+        assert type(timing) is datetime.timedelta
+def test_connection_timeout():
+    with requests_mock.mock() as m:
+        m.get('https://1.1.1.1/', exc=requests.exceptions.ConnectTimeout)
+        test = {'path':'/', 'secure':True, 'verify':True}
+        check = hemApp.Check('test', test)
+        results = check.test_list(["1.1.1.1"])
+        (response, timing) = results[0]
+        assert results is not None
+        assert response == 522
+        assert type(timing) is datetime.timedelta
 
-if __name__ == '__main__':
-    unittest.main()
+def test_check_headers():
+    import hemApp
+    with requests_mock.mock() as m:
+        m.get('https://1.1.1.1/', text="",  request_headers={"host": "example.com"})
+        test = {'path':'/', 'secure':True, 'verify':True, "headers":{"host":"example.com"}}
+        check = hemApp.Check('test', test)
+        results = check.test_list(["1.1.1.1"])
+        (response, timing) = results[0]
+        assert results is not None
+        assert response == 200
+        assert type(timing) is datetime.timedelta
+        test = {'path':'/', 'secure':True, 'verify':True, "headers":{"host":"notexample.com"}}
+        check = hemApp.Check('test', test)
+        try:
+            results = check.test_list(["1.1.1.1"])
+            assert results == []
+        except requests_mock.exceptions.NoMockAddress as m:
+            exit(m)
+            assert "host test" is "host test"
