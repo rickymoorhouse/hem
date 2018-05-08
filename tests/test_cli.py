53ba8c511b1dc8825490ec8d709c438e31d0253b
Ricky Moorhouse <ricky@samespirit.net>
2018-05-08 16:18:11 +0100

    Avoiding endless looping

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -1,57 +1,63 @@
 import unittest
 import requests_mock
+try:
+    from unittest.mock import patch
+except:
+    from mock import patch
 import datetime
 from click.testing import CliRunner
 
-class Basics(unittest.TestCase):
-        def test_cli_version(self):
-            import hemApp.cli
-            runner = CliRunner()
-            result = runner.invoke(hemApp.cli.main, ['--version'])
-            assert "version" in result.output
-        def test_cli_no_config(self):
-            import hemApp
-            import hemApp.cli
-            runner = CliRunner()
-            result = runner.invoke(hemApp.cli.main, ['--config', 'no-config.yaml'])
-            assert result.exit_code == 2
-            #mock_function.assert_called()
-            assert "No config" in result.output
-        def test_cli_config(self):
-            import hemApp
-            import hemApp.cli
+def test_cli_version():
+    import hemApp.cli
+    runner = CliRunner()
+    result = runner.invoke(hemApp.cli.main, ['--version'])
+    assert "version" in result.output
+def test_cli_no_config():
+    import hemApp
+    import hemApp.cli
+    runner = CliRunner()
+    result = runner.invoke(hemApp.cli.main, ['--config', 'no-config.yaml'])
+    assert result.exit_code == 2
+    #mock_function.assert_called()
+    assert "No config" in result.output
+def test_cli_config(mocker):
+    import hemApp
+    import hemApp.cli
+    mocker.patch('hemApp.run_tests', return_value=100)
+    runner = CliRunner()
+    result = runner.invoke(hemApp.cli.main, [
+        '--config', 
+        __file__.replace('test_cli.py','test_cli.yaml')
+        ])
+    assert result.exit_code == -1
+    #mock_function.assert_called()
+    assert hemApp.run_tests.called
+    assert "No config" not in result.output
+
+def test_cli_nosettings(mocker):
+    import hemApp
+    import hemApp.cli
+    mocker.patch('hemApp.run_tests', return_value=100)
+    try:
+        with unittest.assertLogs(level='ERROR') as cm:        
             runner = CliRunner()
             result = runner.invoke(hemApp.cli.main, [
                 '--config', 
-                __file__.replace('test_cli.py','test_cli.yaml')
+                __file__.replace('test_cli.py','test_nosettings.yaml'),
+                '-vv'
                 ])
-            assert result.exit_code == -1
-            #mock_function.assert_called()
-            assert "No config" not in result.output
-
-        def test_cli_nosettings(self):
-            import hemApp
-            import hemApp.cli
-            try:
-                with unittest.assertLogs(level='ERROR') as cm:        
-                    runner = CliRunner()
-                    result = runner.invoke(hemApp.cli.main, [
-                        '--config', 
-                        __file__.replace('test_cli.py','test_nosettings.yaml'),
-                        '-vv'
-                        ])
-                    assert 'ERROR:hemApp.drivers.discovery_file:File not found' in cm.output
-            except AttributeError:
-                runner = CliRunner()
-                result = runner.invoke(hemApp.cli.main, [
-                    '--config', 
-                    __file__.replace('test_cli.py','test_nosettings.yaml'),
-                    '-vv'
-                    ])
-            assert result.exit_code == -1
-            #mock_function.assert_called()
-            assert "No config" not in result.output
-                
+            assert 'ERROR:hemApp.drivers.discovery_file:File not found' in cm.output
+    except AttributeError:
+        runner = CliRunner()
+        result = runner.invoke(hemApp.cli.main, [
+            '--config', 
+            __file__.replace('test_cli.py','test_nosettings.yaml'),
+            '-vv'
+            ])
+    assert hemApp.run_tests.called
+    #mock_function.assert_called()
+    assert "No config" not in result.output
+        
 
 
 
