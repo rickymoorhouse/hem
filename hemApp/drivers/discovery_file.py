b6ff12bda53fd0dc667c12110f85cba1312389d3
Ricky Moorhouse <ricky@samespirit.net>
2020-10-22 13:36:33 +0100

    Allow for keyed objects in file discovery

--- hemApp/drivers/discovery_file.py
+++ hemApp/drivers/discovery_file.py
@@ -14,12 +14,18 @@ def hosts(**kwargs):
         with open(kwargs['name'], 'rt') as source_file:
             hosts = yaml.safe_load(source_file)
             for host in hosts:
+                # if this is just the key then we need the object
+                if isinstance(host, str) and isinstance(hosts, dict):
+                    host_object = hosts[host]
+                else:
+                    host_object = host
+                print(host_object)
                 if 'key' in kwargs:
                     if 'enabled_key' in kwargs:
-                        if host.get(kwargs['enabled_key'], True) == True:
-                            results.append(host.get(kwargs['key']))
+                        if host_object.get(kwargs['enabled_key'], True) == True:
+                            results.append(host_object.get(kwargs['key']))
                     else:
-                        results.append(host.get(kwargs['key']))
+                        results.append(host_object.get(kwargs['key']))
                 else:
                     results.append(host)
     except FileNotFoundError:
